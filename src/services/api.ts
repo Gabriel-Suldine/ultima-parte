8443b06d5ab19b7171a86fb170460b924a302eaa
Gabriel-Suldine <gabriel.suldinne@gmail.com>
2023-04-13 21:01:43 -0400

    parte final

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,12 @@
 import axios from 'axios'
 
-<<<<<<< HEAD
 export const api = axios.create({
     baseURL: 'https://api.github.com/users/',
-=======
-const api = axios.create({
-    baseURL: 'https://api.github.com/',
->>>>>>> 2939c155ee8fc129471d87ebf49ed13ec52ae746
     timeout: 3000,
   });
 
-export async function getProfile(username : String) {
-    let baseURL = `https://api.github.com/users/${username}`
-    const perfil = await api.get(baseURL)
-    return perfil.data
+export async function getProfile(userName:string){
+  const user = await api.get(userName)
+  return user.data.results
 }
-
 
